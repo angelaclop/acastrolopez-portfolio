b933b94b231148b44e4d9e134ed6967e6935b64b
angelaclop <88castroangela@gmail.com>
2020-06-12 17:58:50 -0600

    Content for three projects ready

--- project-vd2.php
+++ project-vd2.php
@@ -23,24 +23,24 @@ require('includes/header.php');
             </div>
             <section>
                 <div class="introSection">
-                <h2 class="h2-VD">3D Jewels –<br>Brand Identity</h2>
+                <h2 class="h2-VD">3D Jewels<br>Brand Identity</h2>
                 <figure class="introImage">
                 <img src="images/hero-3dJewels.jpg" alt="3d Jewels Package Branding"></figure>
                 <p class="intro-VD">3D Jewels is a concept for a custom made jewelry ecommerce, where luxury and technology come together.</p></div>
                 <p>Personal Project	.	#graphic design #branding	.	Oct. 2017, revisited Oct. 2018</p>
                 <h3 class="h3-VD">The Brief</h3>
-                <p class="plainText">The Brief
+                <p class="plainText">
                 Jewelry it’s been one of my hobbies for some time now, I feel attracted to the aesthetics of accessorizing, the materials, and the production process. 
                 Back in the day, making jewelry involved highly manual processes that made it exclusive and expensive. With fast-paced technology advances, it is possible now to produce affordable pieces with endless possibilities of personalization. That is the concept of 3D Jewels, to use advanced manufacturing processes to create beautiful designs that clients can buy from the comfort of their homes. The brand identity for 3D Jewels needed to reflect innovation and technology as well as elegance and sophistication.</p>
                 <h3 class="h3-VD">The Process</h3>
-                <p class="plainText">The initial approach was to select the representative elements of the brand, jewelry, and 3d printing process, and start creating shapes to represent them. From the first sketches, circular shapes representing the front view of a ring with a stone setting started to show something interesting. That idea iterated, incorporating the print head of the 3d printer then transforming the ring into part of the machine surrounding the diamond. Cutting the upper corner of the stone helps to represent better how 3d printing builds objects by successively adding thin layers of material. The following iterations cleaned the shape by working around positive and negative space until creating the symbol of the logo.</p>
+                <p class="plainText">The initial approach was to select the representative elements of the brand, jewelry, and 3d printing process, and start creating shapes to represent them. From the first sketches, circular shapes representing the front view of a ring with a stone setting started to show something interesting. That idea iterated, incorporating the print head of the 3D printer then transforming the ring into part of the machine surrounding the diamond. Cutting the upper corner of the stone helps to represent better how 3D printing builds objects by successively adding thin layers of material. The following iterations cleaned the shape by working around positive and negative space until creating the symbol of the logo.</p>
                 <figure class="singleImage">
                 <figure>
                 <img src="images/3dJewels-sketches.jpg" alt="3d Jewels Logo Sketches">
                 <!-- <img src="images/umbrella.jpg" alt=""> -->
                 </figure><figcaption><em>Sketch iterations of the logo</em></figcaption></figure>
                 <h3 class="h3-VD">The Outcome</h3>
-                <p class="plainText">After the symbol of the logo was ready, the next phase was to work with the brand name to reinforce the concept around 3d Jewels. A mix between serif and sans serif fonts worked well to represent the cleanness of technology and the elegance of jewelry. Ultimately, the color scheme featuring white, teal, and dark gray creates unity between the logo elements and its different variations. Once the visual components of the logo were defined, the brand starts to take form through these elements applied to the package, business cards, and the website.</p>
+                <p class="plainText">After the symbol of the logo was ready, the next phase was to work with the brand name to reinforce the concept around 3D Jewels. A mix between serif and sans serif fonts worked well to represent the cleanness of technology and the elegance of jewelry. Ultimately, the color scheme featuring white, teal, and dark gray creates unity between the logo elements and its different variations. Once the visual components of the logo were defined, the brand starts to take form through these elements applied to the package, business cards, and the website.</p>
                 <figure class="singleImage">
                 <img src="images/3dJewels-logo.jpg" alt="3d Jewels Logo">
                 <figcaption> <em>Primary and secondary logo</em></figcaption>
---
+++ project-vd3.php
@@ -0,0 +1,79 @@
+<?php
+$title = "Angela Castro - Odyssey"; 
+require('includes/header.php');
+ ?>
+	<body>
+        <a href="" id="anchor-header"></a>
+        <header class="standard-header">
+            <a href="index.php" id='logo'><img src="images/ac-logo.svg" alt="Angela Castro Logo"></a>
+            <nav><a href="index.php">home</a>
+            <a href="work.php">work</a>
+            <a href="contact.php">contact</a>
+            <a href="about.php">about</a></nav>
+        </header>
+        <main class="workProject">
+            <div class="subheader">
+                <h1 class='sectionTitle'>Work / Visual Design /</h1>
+                <a href="javascript:history.back()" class="backArrow"><svg xmlns="http://www.w3.org/2000/svg" width="17.27" height="13" viewBox="0 0 23.3 19.19">
+                    <g id="Group_6" data-name="Group 6" transform="translate(-1568.55 -285.166)">
+                      <path id="Path_17" class="arrow" data-name="Path 17" d="M7670.6,298.8h-20.8l7.8-7.867" transform="translate(-6080 -4)" fill="none" stroke="#474747" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5"/>
+                      <path id="Path_18" class="arrow" data-name="Path 18" d="M7652.762,293.875l3.775,3.712" transform="translate(-6079 5)" fill="none" stroke="#474747" stroke-linecap="round" stroke-width="2.5"/>
+                    </g>
+                  </svg> <span>back</span></a>
+            </div>
+            <section>
+                <div class="introSection">
+                <h2 class="h2-VD">Odyssey<br>Brand Identity</h2>
+                <figure class="introImage">
+                <img src="images/odyssey-cup.jpg" alt="Odyssey Cups"></figure>
+                <p class="intro-VD">The Odyssey coffeehouse rejoices in a cozy atmosphere and a unique coffee concept. I embarked on the adventure of creating a new branding concept for this coffee shop.</p></div>
+                <p>Personal Project    .    #graphic design #branding    .    April 2019</p>
+                <h3 class="h3-VD">The Brief</h3>
+                <p class="plainText">While studying, I worked as a Barista for The Odyssey. During this time, I got to love coffee even more, if that’s possible for someone born in Colombia. I was fascinated by the skills and empathy that intervene in the preparation of a good cup of coffee. Empathy is what customers value when they get their perfect skim-decaf-latte in the morning and it was empathy that drove my interest in working with the brand of this coffee house.</p>
+                <p class="plainText">The Odyssey is located on SAIT's main campus in Calgary, is in the heart of students, faculty, and anyone that has visited, it offers ethically sourced coffee of the best quality and their expertise for coffee is renowned. On their first decade anniversary, the brand called for a refresh, although they had an agency to work on that, I wanted to land my inspiration and insight into an alternative design proposal.</p>
+                <h3 class="h3-VD">The Process</h3>
+                <figure class="singleImage">
+                <img src="images/odyssey-Bstorming.jpg" alt="Logo Brainstorming Process">
+                <figcaption> <em>Brainstorming process</em></figcaption>
+                </figure>
+                <p class="plainText">There were some places to start the ideation process. First, the name Odyssey itself brings to memory some images of adventures, old ships, and mythical gods. On the other hand, I wanted to represent the essence of the place: a throbbing passion for coffee.</p>
+                <p class="plainText">Before jumping into sketching, I started by writing some descriptive words, then grouping and connecting them to get some ideas that lead the sketches into more solid concepts.</p>
+                <figure class="singleImage">
+                <img src="images/odyssey-sketches.jpg" alt="Logo Sketches Odyssey Coffehouse">
+                <figcaption> <em>Sketches of logo concepts</em></figcaption>
+                </figure>
+                <h3 class="h3-VD">The Outcome</h3>
+                <p class="plainText">After the symbol of the logo was ready, the next phase was to work with the brand name to reinforce the concept around 3d Jewels. A mix between serif and sans serif fonts worked well to represent the cleanness of technology and the elegance of jewelry. Ultimately, the color scheme featuring white, teal, and dark gray creates unity between the logo elements and its different variations. Once the visual components of the logo were defined, the brand starts to take form through these elements applied to the package, business cards, and the website.</p>
+                <figure class="singleImage">
+                <img src="images/odyssey-logo.jpg" alt="Odyssey Logo">
+                <figcaption> <em>Primary and secondary logo</em></figcaption>
+                </figure>
+                <p class="plainText">The color selection featuring blue and brown in different shades helped to create contrast and highlight small details. This palette provided enough variations to be applied in collateral pieces, like the poster below.</p>
+                <figure class="singleImage">
+                <img src="images/odyssey-poster.jpg" alt="Odyssey Poster Design">
+                <figcaption> <em>Pop art style poster from different logo color applications</em></figcaption>
+                </figure>
+                <figure class="singleImage">
+                <img src="images/odyssey-card.jpg" alt="Odyssey Loyalty Card">
+                <figcaption> <em>Odyssey Customer Loyalty Card</em></figcaption>
+                </figure>
+            </section>
+            <div id="end-mark"><img src="images/icons/ac-icon.svg" alt="Angela Castro symbol" ></div>
+            <div id='arrow-links'>
+                <a href="javascript:history.back()" class="backArrow"><svg xmlns="http://www.w3.org/2000/svg" width="17.27" height="13" viewBox="0 0 23.3 19.19">
+                    <g id="Group_7" data-name="Group 7" transform="translate(-1568.55 -285.166)">
+                      <path id="Path_19" class="arrow" data-name="Path 19" d="M7670.6,298.8h-20.8l7.8-7.867" transform="translate(-6080 -4)" fill="none" stroke="#474747" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5"/>
+                      <path id="Path_20" class="arrow" data-name="Path 20" d="M7652.762,293.875l3.775,3.712" transform="translate(-6079 5)" fill="none" stroke="#474747" stroke-linecap="round" stroke-width="2.5"/>
+                    </g>
+                  </svg> <span>back</span></a>
+                  <a href="#anchor-header" id="upArrow"><svg xmlns="http://www.w3.org/2000/svg" width="13" height="17.27" viewBox="0 0 19.19 23.3">
+                    <g id="Group_8" data-name="Group 8" transform="translate(304.355 -1568.55) rotate(90)">
+                      <path id="Path_16" data-name="Path 16" d="M7670.6,298.8h-20.8l7.8-7.867" transform="translate(-6080 -4)" fill="none" stroke="#474747" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5"/>
+                      <path id="Path_15" data-name="Path 15" d="M7652.762,293.875l3.775,3.712" transform="translate(-6079 5)" fill="none" stroke="#474747" stroke-linecap="round" stroke-width="2.5"/>
+                    </g>
+                  </svg><span>up</span></a>
+                </div>
+        </main>
+        <?php
+require('includes/footer.php');
+ ?>
