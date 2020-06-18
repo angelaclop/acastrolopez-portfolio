a4b13531e3ff215d2a3b47f83f0e71d391f5bc70
angelaclop <88castroangela@gmail.com>
2020-06-18 13:41:23 -0600

    This is master including VD and PD sections, navigation ok

--- includes/nav.php
+++ includes/nav.php
@@ -1,8 +1,7 @@
 <header class="standard-header">
             <a href="index.php" id='logo'><img src="images/ac-logo.svg" alt="Angela Castro Logo"></a>
             <nav><a href="index.php">home</a>
-            <!--When PD section is published enable this link--><!--<a href="work.php">work</a>-->
-            <!--...and disable this--><a href="visual-design.php">work</a>
+            <a href="work.php">work</a>
             <a href="contact.php">contact</a>
             <a href="about.php">about</a></nav>
         </header>
